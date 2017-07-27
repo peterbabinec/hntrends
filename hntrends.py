f845dc0f9d85d7dd9b6263edfb04faa525913fd3
xbabinec <babinec.peter@gmail.com>
2017-07-27 15:10:11 +0000

    Initial deployed version

---
+++ hntrends.py
@@ -0,0 +1,9 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "<h1>This is Hacker News Trends</h1>"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
